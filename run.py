b3ac07ca8f23f8ca67a04a5e184a18dcca3366bc
James Hong <james.hong@cs.stanford.edu>
2020-08-20 12:44:24 -0700

    Misc

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ import cv2
 labels = [
     {'name': 'action', 'color': (0, 255, 0)},
     {'name': 'replay', 'color': (255, 0, 0)},
-    {'name': 'other', 'color': (0, 0, 255)}
+    {'name': 'other', 'color': (0, 0, 255)},
+    {'name': 'toss', 'color': (0, 255, 255)},
 ]
 
 parser = argparse.ArgumentParser()
@@ -17,23 +18,24 @@ parser.add_argument('out_file')
 parser.add_argument('-cl', type=float, default=5)
 args = parser.parse_args()
 
-
 video_file = args.video
 video_basename = os.path.basename(video_file)
 video_name, ext = os.path.split(video_basename)
 out_file = args.out_file
 clip_dir = 'clips-' + video_basename
 
-vc = cv2.VideoCapture(video_file)
-fps = vc.get(cv2.CAP_PROP_FPS)
-vc.release()
-clip_frames = round(fps * args.cl)
-print('Clips contain {} frames'.format(clip_frames))
-
 annotator = Annotator(
     labels, clip_dir, annotation_file=out_file, N_show_approx=100)
 
 if not os.path.exists(clip_dir):
+    vc = cv2.VideoCapture(video_file)
+    fps = vc.get(cv2.CAP_PROP_FPS)
+    vc.release()
+    clip_frames = round(fps * args.cl)
+    print('Clips contain {} frames'.format(clip_frames))
+    with open(os.path.join(clip_dir, 'frame_count.txt'), 'w') as fp:
+        fp.write(str(clip_frames))
+
     os.makedirs(clip_dir)
     annotator.video_to_clips(
         video_file, clip_dir, clip_length=clip_frames, overlap=0,
